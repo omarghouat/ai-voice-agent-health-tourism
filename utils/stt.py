347d086eb8afaf260eb158cf6eaf39fd313e81cc
omarghouat <omarghouat@gmail.com>
2025-05-30 12:38:48 +0100

    Create stt.py

---
+++ utils/stt.py
@@ -0,0 +1,18 @@
+import vosk
+import pyaudio
+
+def listen():
+    model = vosk.Model("model-en")  # Make sure this folder exists in root
+    recognizer = vosk.KaldiRecognizer(model, 16000)
+
+    mic = pyaudio.PyAudio()
+    stream = mic.open(rate=16000, format=pyaudio.paInt16, channels=1,
+                      input=True, frames_per_buffer=8192)
+    stream.start_stream()
+
+    print("🎤 Listening...")
+    while True:
+        data = stream.read(4096)
+        if recognizer.AcceptWaveform(data):
+            result = recognizer.Result()
+            return result[14:-3]  # Extract text from JSON result
