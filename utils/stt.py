ea42e410be02f56f1e6da3fa469950d8bab72f7f
omarghouat <omarghouat@gmail.com>
2025-05-30 20:57:54 +0100

    Update stt.py

--- utils/stt.py
+++ utils/stt.py
@@ -1,5 +1,7 @@
 import vosk
 #import pyaudio
+import av
+import numpy as np
 
 def listen():
     model = vosk.Model("model-en")  # Make sure this folder exists in root
@@ -16,3 +18,10 @@ def listen():
         if recognizer.AcceptWaveform(data):
             result = recognizer.Result()
             return result[14:-3]  # Extract text from JSON result
+
+    def audio_callback(frame: av.AudioFrame) -> av.AudioFrame:
+    audio_data = frame.to_ndarray()
+    # Here you can process or save the audio_data
+    print("Audio shape:", audio_data.shape)
+    return frame  # Must return the frame even if not modified
+
