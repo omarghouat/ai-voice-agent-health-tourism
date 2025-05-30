bcf661b104f50de74a306752781b4ee1cfb4bb40
omarghouat <omarghouat@gmail.com>
2025-05-30 20:56:12 +0100

    Update stt.py

--- utils/stt.py
+++ utils/stt.py
@@ -1,11 +1,11 @@
 import vosk
-import pyaudio
+#import pyaudio
 
 def listen():
     model = vosk.Model("model-en")  # Make sure this folder exists in root
     recognizer = vosk.KaldiRecognizer(model, 16000)
 
-    mic = pyaudio.PyAudio()
+    #mic = pyaudio.PyAudio()
     stream = mic.open(rate=16000, format=pyaudio.paInt16, channels=1,
                       input=True, frames_per_buffer=8192)
     stream.start_stream()
